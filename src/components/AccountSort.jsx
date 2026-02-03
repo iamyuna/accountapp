4d8ef72025aab453c0368501c3da5031b6c7131b
yuna park <dbsdk95331@naver.com>
2026-02-03 18:04:01 +0900

    정렬 텍스트 수정

--- src/components/AccountSort.jsx
+++ src/components/AccountSort.jsx
@@ -2,7 +2,7 @@ export default function AccountSort({sortList, toggleSort}){
 
     return(
         <button onClick={toggleSort} className="w-[70px] h-[40px] text-[0.95rem] text-white bg-[#B0A5FF] px[7px] rounded-[5px] ml-[5px]">
-            {sortList === "newest" ? "최신순" : "오래된순"}
+            {sortList === "newest" ? "오래된순" : "최신순"}
         </button>
     );
 }
